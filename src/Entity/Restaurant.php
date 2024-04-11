cc46aac437c35a98787f4321a828897ff51ad624
fxdstudiokg <fxd@studiokg.fr>
2024-04-11 15:34:46 +0200

    creation de l entité restaurant

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\TimestampableTrait;
+use App\Repository\RestaurantRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RestaurantRepository::class)]
+class Restaurant
+{
+    use TimestampableTrait;
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?array $amOpeningTime = null;
+
+    #[ORM\Column]
+    private array $pmOpeningTime = [];
+
+    #[ORM\Column]
+    private ?int $maxGuest = 0;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAmOpeningTime(): ?array
+    {
+        return $this->amOpeningTime;
+    }
+
+    public function setAmOpeningTime(?array $amOpeningTime): static
+    {
+        $this->amOpeningTime = $amOpeningTime;
+
+        return $this;
+    }
+
+    public function getPmOpeningTime(): array
+    {
+        return $this->pmOpeningTime;
+    }
+
+    public function setPmOpeningTime(array $pmOpeningTime): static
+    {
+        $this->pmOpeningTime = $pmOpeningTime;
+
+        return $this;
+    }
+
+    public function getMaxGuest(): ?int
+    {
+        return $this->maxGuest;
+    }
+
+    public function setMaxGuest(int $maxGuest): static
+    {
+        $this->maxGuest = $maxGuest;
+
+        return $this;
+    }
+}
